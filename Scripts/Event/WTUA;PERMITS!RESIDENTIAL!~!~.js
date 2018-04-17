62fc9b2586de21c2d86c774dd3340d1c7579a565
Corey Cooper <coreyccooper@gmail.com>
2018-04-17 14:43:04 -0700

    Add files via upload

---
+++ Scripts/Event/WTUA;PERMITS!RESIDENTIAL!~!~.js
@@ -0,0 +1,5 @@
+showDebug = true;
+
+if (wfTask == "Application Acceptance" && wfStatus == "Accepted - Plan Review Req") {
+logDebug("This should be activating task - WTUA:Permits/Residential/*/*");
+}
