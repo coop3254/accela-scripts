c7b04d4112e527a4694404fa9487fe1b64a2abe3
Corey Cooper <coreyccooper@gmail.com>
2018-04-27 13:33:58 -0700

    Add files via upload
    Added invoiceFee function

--- Scripts/Event/WTUA;PERMITS!~!~!~.js
+++ Scripts/Event/WTUA;PERMITS!~!~!~.js
@@ -2,3 +2,7 @@
 if (wfTask == "Application Acceptance" && wfStatus == "Accepted - Plan Review Req") {
 	scheduleInspection("Site Visit", 5);
 }
+
+if (wfTask == "Inspection" && wfStatus == "Failed Inspection") {
+	invoiceFee("PMT_060FAIL", "FINAL");
+}
