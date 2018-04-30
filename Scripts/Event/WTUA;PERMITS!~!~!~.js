da9a13d023dc7182a90740ad00159ff1eb6c6d22
Corey Cooper <coreyccooper@gmail.com>
2018-04-30 13:14:26 -0700

    Add files via upload

--- Scripts/Event/WTUA;PERMITS!~!~!~.js
+++ Scripts/Event/WTUA;PERMITS!~!~!~.js
@@ -1,11 +1,8 @@
 
-showDebug=3;
-
 if (wfTask == "Application Acceptance" && wfStatus == "Accepted - Plan Review Req") {
 	scheduleInspection("Site Visit", 5);
 }
 
 if (wfTask == "Inspection" && wfStatus == "Failed Inspection") {
-	logDebug("working");
 	addFee("PMT_060FAIL", "PMT_GENERAL", "FINAL", 1, "Y");
 }
