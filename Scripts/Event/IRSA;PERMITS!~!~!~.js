e15b95bd693b018d3b69a3cc990d153be4793b3e
Corey Cooper <coreyccooper@gmail.com>
2018-05-30 09:21:40 -0700

    added sendInspectionResultGuidesheetReport()

--- Scripts/Event/IRSA;PERMITS!~!~!~.js
+++ Scripts/Event/IRSA;PERMITS!~!~!~.js
@@ -2,7 +2,7 @@ if (inspType == 'Annual Apartment Inspection' && inspResult == 'Fail Inspection'
 	closeTask('Annual Inspection', 'Failed Inspection', '', '');
 	activateTask('Reinspection 30 Days');
 	scheduleInspection('Annual Apartment Inspection', 30, 'ADMIN');
-	emailContact("Inspection Results", "Your inspection " + inspType + " has failed.", "Applicant");
+	sendInspectionResultGuidesheetReport();
 }
 
 if (inspType == 'Annual Apartment Inspection' && inspResult == 'Fail ReInspection') {
@@ -10,17 +10,16 @@ if (inspType == 'Annual Apartment Inspection' && inspResult == 'Fail ReInspectio
 	activateTask('Violation Inspection 15 Days');
 	scheduleInspection('Annual Apartment Inspection', 15, 'ADMIN');
 	addFee("PMT_060FAIL", "PMT_GENERAL", "FINAL", 1, "Y");
-	emailContact("Inspection Results", "Your inspection " + inspType + " has failed the re-inspection.", "Applicant");
+	sendInspectionResultGuidesheetReport();
 }
 
 if (inspType == 'Annual Apartment Inspection' && inspResult == 'Fail 2nd Reinspection') {
 	closeTask('Violation Inspection 15 Days', 'Failed Violation Inspection', '', '');
 	activateTask('City Attorney');
-	emailContact("Inspection Results", "Your inspection " + inspType + " has failed the second re-inspection.", "Applicant");
+	sendInspectionResultGuidesheetReport();
 }
 
 if (inspType == 'Annual Apartment Inspection' && inspResult == 'Pass') {
 	closeTask('Annual Inspection', 'Passed');
 	activateTask('Final Review');
-	emailContact("Inspection Results", "Your inspection " + inspType + " has passed.", "Applicant");
 }
