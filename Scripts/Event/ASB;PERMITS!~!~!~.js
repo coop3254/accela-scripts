e38d858648bcfe6a6756d8c0e5a95a9815590ffa
Corey Cooper <coreyccooper@gmail.com>
2018-10-08 12:15:34 -0700

    Update ASB;PERMITS!~!~!~.js

--- Scripts/Event/ASB;PERMITS!~!~!~.js
+++ Scripts/Event/ASB;PERMITS!~!~!~.js
@@ -1,5 +1,5 @@
 if (parcelConditionExistsASB("Building Permit")) {
 	cancel = true;
 	showMessage = true;
-	comment('<font color=red><b>Their is a a condition on this parcel that will not allow a permit to be submitted.</b></font>');
+	comment('<font color=red><b>There is a condition on this parcel that will not allow a permit to be submitted.</b></font>');
 }
