950b56bda9c97a2da41ee1fe3b9a7edefa4afc04
Corey Cooper <coreyccooper@gmail.com>
2018-10-18 00:30:05 -0700

    Update WTUA;PLANNING!APPLICATION!PLANNED UNIT DEVELOPMENT!~.js

--- Scripts/Event/WTUA;PLANNING!APPLICATION!PLANNED UNIT DEVELOPMENT!~.js
+++ Scripts/Event/WTUA;PLANNING!APPLICATION!PLANNED UNIT DEVELOPMENT!~.js
@@ -1,6 +1,6 @@
 if (wfTask == "City Council Ratify" && wfStatus == "Approved") {
 logDebug("Creating child records!");
-createChild("Permits","Grading","NA","NA");
-createChild("Permits","Public Works","Encroachment","Permit");
-createChild("Permits","Commercial","New","NA");
+createChild("Permits","Grading","NA","NA","Grading Permit");
+createChild("Permits","Public Works","Encroachment","Permit","Encroachment Permit");
+createChild("Permits","Commercial","New","NA","Commercial New Building Permit");
 }
