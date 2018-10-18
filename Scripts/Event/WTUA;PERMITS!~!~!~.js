46985ae824555128f098bf71fc470fd7b69a65f3
Corey Cooper <coreyccooper@gmail.com>
2018-10-18 00:33:56 -0700

    Update WTUA;PERMITS!~!~!~.js

--- Scripts/Event/WTUA;PERMITS!~!~!~.js
+++ Scripts/Event/WTUA;PERMITS!~!~!~.js
@@ -6,3 +6,7 @@ if (wfTask == "Application Acceptance" && wfStatus == "Accepted - Plan Review Re
 if (wfTask == "Inspection" && wfStatus == "Failed Inspection") {
 	addFee("PMT_060FAIL", "PMT_GENERAL", "FINAL", 1, "Y");
 }
+
+if (wfTask == "Permit Issuance" && wfStatus == "Issued") {
+	addFee("PMT_001", "PMT_GENERAL", "FINAL", 4, "Y");
+}
