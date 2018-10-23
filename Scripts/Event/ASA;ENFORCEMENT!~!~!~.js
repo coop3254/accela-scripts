507b95950e2109e9d01327aeae6a0af98e733167
Corey Cooper <coreyccooper@gmail.com>
2018-10-22 19:11:38 -0700

    Add files via upload

---
+++ Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
@@ -0,0 +1,7 @@
+if (appMatch('Enforcement/Incident/Abatement/Abandoned Vehicle')) {
+	addFee("ENF-20", "ENF_GENERAL", "FINAL", 1, "Y");
+}
+
+if (appMatch('Enforcement/Incident/Abatement/Weeds')) {
+	addFee("ENF-30", "ENF_GENERAL", "FINAL", 1, "Y");
+}
