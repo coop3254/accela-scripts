ddf480e4baad2fa872a14696c4f5709b472684b4
Corey Cooper <coreyccooper@gmail.com>
2019-01-21 13:47:27 -0800

    Add files via upload

--- Scripts/Event/WTUA;PERMITS!~!~!~.js
+++ Scripts/Event/WTUA;PERMITS!~!~!~.js
@@ -1,3 +1,7 @@
+if (appMatch('Permits/Residential/New/NA') && wfTask == "Permit Issuance" && wfStatus == "Issued") {
+	addFee("PMT_ISS_FEE", "PMT_GENERAL", "FINAL", 1, "Y");
+}
+
 if (wfTask == "Application Acceptance" && wfStatus == "Accepted - Plan Review Req") {
 	scheduleInspection("Site Visit", 5);
 }
@@ -5,11 +9,12 @@ if (wfTask == "Application Acceptance" && wfStatus == "Accepted - Plan Review Re
 if (wfTask == "Inspection" && wfStatus == "Failed Inspection") {
 	addFee("PMT_060FAIL", "PMT_GENERAL", "FINAL", 1, "Y");
 }
-
+/*
 if (wfTask == "Permit Issuance" && wfStatus == "Issued") {
 	addFee("PMT_001", "PMT_GENERAL", "FINAL", 4, "Y");
 	email("mhopkins@accela.com","mhopkins@accela.com","Permit Ready","Your permit has been issued and is ready for pickup.");
 }
+*/
 
 if (appMatch('Permits/Commercial/New/NA') && wfTask == "Application Submittal" && wfStatus == "Accepted - Plan Review Req") {
 	activateTask("Fire Review");
