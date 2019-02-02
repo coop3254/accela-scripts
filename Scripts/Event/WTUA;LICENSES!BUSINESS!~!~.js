2b90439846083e97b984838445e7a1dc122c68b1
Corey Cooper <coreyccooper@gmail.com>
2019-02-01 17:33:51 -0800

    Add files via upload

--- Scripts/Event/WTUA;LICENSES!BUSINESS!~!~.js
+++ Scripts/Event/WTUA;LICENSES!BUSINESS!~!~.js
@@ -1,3 +1,4 @@
 if ((appMatch('Licenses/Business/Restaurant/Application')) && isTaskActive('Zoning Review') || isTaskActive('Building Review')) {
+	logDebug("Email should fire off!");
 	email("airkulla@accela.com","airkulla@accela.com","Ready for Review","Please perform the necessary task that has been assigned to you.");
 }
