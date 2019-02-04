2f0321bc2816cc8bbc2eca685e4744385de42844
Corey Cooper <coreyccooper@gmail.com>
2019-02-04 14:28:26 -0800

    Update IRSA;BUILDING!~!~!~.js

--- Scripts/Event/IRSA;BUILDING!~!~!~.js
+++ Scripts/Event/IRSA;BUILDING!~!~!~.js
@@ -1,4 +1,5 @@
 if (inspType == 'Annual Fire Safety Inspection' && inspResult == 'Fail Inspection') {
 	scheduleInspection('Annual Fire Safety Reinspection', 5);
 	addFee("BLDGGEN05", "BLDG_GENERAL", "FINAL", 1, "Y");
+	addFee("BLDGGEN09", "BLDG_GENERAL", "FINAL", 1, "Y");
 }
