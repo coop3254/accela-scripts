e7e27e1add274a159ef1a9f18b5042eae31c87bd
Corey Cooper <coreyccooper@gmail.com>
2019-02-04 00:29:16 -0800

    Update WTUA;BUILDING!FIRE!OCCUPANCY!~.js

--- Scripts/Event/WTUA;BUILDING!FIRE!OCCUPANCY!~.js
+++ Scripts/Event/WTUA;BUILDING!FIRE!OCCUPANCY!~.js
@@ -1,3 +1,4 @@
 if (wfTask == "Administrative Review" && wfStatus == "Ready for Annual Inspection") {
 	scheduleInspection("Annual Fire Safety Inspection", 365);
+	addFee("BLDGGEN07", "BLDG_GENERAL", "FINAL", 1, "Y");
 }
