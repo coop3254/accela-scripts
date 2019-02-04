37c05271d41a1378abcf86478a304a9e58a6849a
Corey Cooper <coreyccooper@gmail.com>
2019-02-03 23:38:57 -0800

    Update IRMA;BUILDING!~!~!~.js

--- Scripts/Event/IRMA;BUILDING!~!~!~.js
+++ Scripts/Event/IRMA;BUILDING!~!~!~.js
@@ -1,4 +1,4 @@
-if (inspType == 'Annual Fire Safety Inspection' && inspResult == 'Fail Inspection') {
-	scheduleInspection('Annual Fire Safety Reinspection', 5);
+if (inspType == "Annual Fire Safety Inspection" && inspResult == "Fail Inspection") {
+	scheduleInspection("Annual Fire Safety Reinspection", 5);
 	addFee("BLDGGEN05", "BLDG_GENERAL", "FINAL", 1, "Y");
 }
