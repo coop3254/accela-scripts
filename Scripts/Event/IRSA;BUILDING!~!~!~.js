cf7eae501158d1588c40e95872f493deaafd47a7
Corey Cooper <coreyccooper@gmail.com>
2019-02-03 23:23:32 -0800

    Update IRSA;BUILDING!~!~!~.js

--- Scripts/Event/IRSA;BUILDING!~!~!~.js
+++ Scripts/Event/IRSA;BUILDING!~!~!~.js
@@ -1,3 +1,4 @@
-if (inspType == 'Progress Check' && inspResult == 'Passed') {
-	scheduleInspection('Progress Check', 365, 'AIRKULLA');
+if (inspType == 'Annual Fire Safety Inspection' && inspResult == 'Fail Inspection') {
+	scheduleInspection('Annual Fire Safety Reinspection', 5);
+	addFee("BLDGGEN05", "BLDG_GENERAL", "FINAL", 1, "Y");
 }
