d4fdf2810dad2df633a36a92f6bc5e2df9abe91e
Corey Cooper <coreyccooper@gmail.com>
2019-02-09 22:19:21 -0800

    Add files via upload

--- Scripts/Event/ASB;ENFORCEMENT!~!~!~.js
+++ Scripts/Event/ASB;ENFORCEMENT!~!~!~.js
@@ -1,4 +1,4 @@
 if (parcelConditionExistsASB("Flood Zone")) {
-	addFee("ENF-40", "ENF_GENERAL", "FINAL", 1, "Y");
+	addFee("ENF_50", "ENF_GENERAL", "FINAL", 1, "Y");
 	comment('<font color=red><b>There is a condition on this parcel that will cause a violation fee to be assessed.</b></font>');
 }
