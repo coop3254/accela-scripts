1fc5876363c676f7099f7eafb77f2440807166bf
Corey Cooper <coreyccooper@gmail.com>
2019-02-11 11:41:44 -0800

    Update ASA;ENFORCEMENT!~!~!~.js

--- Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
+++ Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
@@ -5,3 +5,7 @@ if (appMatch('Enforcement/Incident/Abatement/Abandoned Vehicle')) {
 if (appMatch('Enforcement/Incident/Abatement/Weeds')) {
 	addFee("ENF-30", "ENF_GENERAL", "FINAL", 1, "Y");
 }
+
+if((appMatch('Enforcement/*/*/*')) {
+	addFee("ENF-20", "ENF_GENERAL", "FINAL", 1, "Y");
+}
