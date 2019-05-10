abb5fb8ab4263ac5736ec0598c55b2108584bd3b
Corey Cooper <coreyccooper@gmail.com>
2019-05-10 16:06:58 -0700

    Add files via upload

--- Scripts/Event/ASA;PERMITS!~!~!~.js
+++ Scripts/Event/ASA;PERMITS!~!~!~.js
@@ -1,3 +1,11 @@
 if (appMatch('Permits/Commercial/New/NA') && AInfo["Total Floor Area"] != null) {
   addFee("PMT_070", "PMT_GENERAL", "FINAL", AInfo["Total Floor Area"], "Y");
 }
+
+var isCreatedByACA = cap.isCreatedByACA();
+
+if (isCreatedByACA && appMatch('Permits/Residential/Re-Roof/NA')) {
+	updateAppStatus('Issued', "Permit Issued via ACA");
+	closeTask('Application Submittal', 'Accepted');
+	updateTask('Case Closed', 'Issued');
+}
