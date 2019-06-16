fc480ee2321a510d8e6e1f4ae6b1e83dd97a5cb7
Corey Cooper <coreyccooper@gmail.com>
2019-06-16 14:43:25 -0700

    Add files via upload

--- Scripts/Event/WTUA;LICENSES!~!~!~.js
+++ Scripts/Event/WTUA;LICENSES!~!~!~.js
@@ -2,3 +2,7 @@ if (wfTask == "License Issuance" && wfStatus == "Issued") {
 logDebug("Creating child record!");
 createChild("Licenses","Teaching Certificate","Initial","License","Teaching Initial Certificate License");
 }
+
+if (appMatch('Licenses/Engineers & Architects/Engineer/Application') && wfTask == "Issuance" && wfStatus == "Issued") {
+	createChild("Licenses","Engineers & Architects","Engineer","License","Engineer License")
+}
